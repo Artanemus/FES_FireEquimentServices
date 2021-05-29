e27508c2e18eba98bb80acfc5994fba109e73517
Artanemus <ambrose.ben@gmail.com>
2021-05-29 14:17:39 +1000

    Spelling mistake

---
+++ SCRIPTS/005_ChemicalType_Symbol.sql
@@ -0,0 +1,7 @@
+USE [IDFES]
+go
+
+-- Standard Alter Table SQL
+
+ALTER TABLE dbo.ChemicalType ADD Symbol nvarchar(50)  NULL
+go
--- SCRIPTS/006_ChemicalType_Symbol.sql
+++
@@ -1,7 +0,0 @@
-USE [IDFES]
-go
-
--- Standard Alter Table SQL
-
-ALTER TABLE dbo.ChemicalType ADD Sysmbol nvarchar(50)  NULL
-go
